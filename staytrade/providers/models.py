4cb21694f8b91e3186211d59a1ecdcf8673a7851
villuis <villuis91@icloud.com>
2025-02-28 18:29:58 +0100

    Providers first model scratch

--- staytrade/providers/models.py
+++ staytrade/providers/models.py
@@ -1,1 +1,28 @@
+from staytrade.shared.abstract_models import TimeStampedModel
+from staytrade.users.models import User, EnterpriseAccount
 from django.db import models
+
+
+class Hotel(TimeStampedModel):
+    class HotelTypes(models.TextChoices):
+        PROVIDER = "provider", _("Provider")
+        TRADER = "trader", _("Trader")
+
+    user = models.ForeignKey(User)
+    account = models.ForeignKey(
+        EnterpriseAccount, blank=True, null=True, on_delete=models.SET_NULL
+    )
+    description = models.TextField()
+    stars = models.IntegerField()
+
+
+class RoomTypes(TimeStampedModel):
+    pass
+
+
+class Rooms(TimeStampedModel):
+    pass
+
+
+class RoomNight(TimeStampedModel):
+    pass
---
+++ staytrade/shared/abstract_models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    ``created_at`` and ``updated_at`` fields.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
